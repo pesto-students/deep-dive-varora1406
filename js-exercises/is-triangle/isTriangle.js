aa6d627e9ff63b7ac0c2b464bd185e7c670d1e57
Vaibhav <varora@catalystone.com>
2020-04-24 21:17:40 +0530

    Update program with some betterments, which are : - Using array utilities to reduce LOC in number utilities created in this file - change some naming

--- js-exercises/is-triangle/isTriangle.js
+++ js-exercises/is-triangle/isTriangle.js
@@ -1,36 +1,12 @@
-function isNumber(...args) {
-  let result = true;
+const isNumber = (arg) => typeof arg === 'number';
 
-  for (const arg of args) {
-    if (typeof arg !== 'number') {
-      result = false;
-      break;
-    }
-  }
+const isPositiveNumber = (arg) => isNumber(arg) && (arg > 0);
 
-  return result;
-}
+const arePositiveNumbers = (args) => args.every(isPositiveNumber);
 
-function isPositiveInteger(...args) {
-  let result = true;
-
-  for (const arg of args) {
-    if (arg !== Math.abs(arg)) {
-      result = false;
-      break;
-    }
-  }
-
-  return result;
-}
-
-function isTriangle(side1, side2, side3) {
-  if (!isNumber(side1, side2, side3)) {
-    throw TypeError(`program expect all sides numeric, instead got ${side1}, ${side2}, ${side3}`);
-  }
-
-  if (!isPositiveInteger(side1, side2, side3)) {
-    throw Error(`program expect all sides positive, instead got ${side1}, ${side2}, ${side3}`);
+const isTriangle = (side1, side2, side3) => {
+  if (!arePositiveNumbers(side1, side2, side3)) {
+    throw Error(`program expect all sides positive numbers, instead got ${side1}, ${side2}, ${side3}`);
   }
 
   if (side1 + side2 <= side3) {
@@ -41,12 +17,8 @@ function isTriangle(side1, side2, side3) {
     return false;
   }
 
-  if (side3 + side1 <= side2) {
-    return false;
-  }
-
-  return true;
-}
+  return (side3 + side1 <= side2);
+};
 
 export {
   isTriangle,
