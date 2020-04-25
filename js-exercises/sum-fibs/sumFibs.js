428516f122905e6f152e05327496f5c391ec4c2c
Vaibhav <varora@catalystone.com>
2020-04-25 20:30:04 +0530

    Add sum-fibs implementation using added utilities

--- js-exercises/sum-fibs/sumFibs.js
+++ js-exercises/sum-fibs/sumFibs.js
@@ -2,6 +2,12 @@ const isNumber = (arg) => typeof arg === 'number';
 
 const isOddNumber = (arg) => isNumber(arg) && (arg % 2 !== 0);
 
+function* returnNumberGenerator(number) {
+  while (true) {
+    yield number;
+  }
+}
+
 function* fibonacciGenerator(startNumber) {
   let [currentNumber, previousNumber] = returnNumberGenerator(startNumber);
 
@@ -14,14 +20,24 @@ function* fibonacciGenerator(startNumber) {
   }
 }
 
-function* returnNumberGenerator(number) {
-  while (true) {
-    yield number;
-  }
-}
 
 const sumFibs = (endNumber) => {
+  const startNumber = 1;
+  let result = startNumber;
 
+  const fibonacciIterator = fibonacciGenerator(startNumber);
+
+  for (const number of fibonacciIterator) {
+    if (number > endNumber) {
+      break;
+    }
+
+    if (isOddNumber(number)) {
+      result += number;
+    }
+  }
+
+  return result;
 };
 
 export {
