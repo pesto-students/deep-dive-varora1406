9cbd808493c0e13910590e0e240fcaaa8ac5e4e6
Vaibhav <varora@catalystone.com>
2020-04-25 23:46:37 +0530

    #6 Remove setTimeout from test, because it doesn't return number in nodejs

--- js-exercises/sum-primes/sumPrimes.test.js
+++ js-exercises/sum-primes/sumPrimes.test.js
@@ -3,7 +3,6 @@ import { sumPrimes } from './sumPrimes';
 describe('sumPrimes', () => {
   test('should return a number', () => {
     expect(typeof sumPrimes(10)).toEqual('number');
-    expect(typeof sumPrimes(setTimeout(() => { }, 500))).toEqual('number');
   });
   test('should return the correct output', () => {
     expect(sumPrimes(10)).toBe(17);
