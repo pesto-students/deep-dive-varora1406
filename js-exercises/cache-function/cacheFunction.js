a5c2a50ed881037a34713ae4e4798ecee349512e
Vaibhav <varora@catalystone.com>
2020-04-26 22:11:35 +0530

    #10 submit TODO's to except in upcoming commits

---
+++ js-exercises/cache-function/cacheFunction.js
@@ -0,0 +1,11 @@
+const cloneDeep = (object) => {
+  // TODO: will complete once strategy is there
+};
+
+function cacheFunction(functionToCache) {
+  // TODO: Once cloneDeep is completed
+}
+
+export {
+  cacheFunction,
+};
