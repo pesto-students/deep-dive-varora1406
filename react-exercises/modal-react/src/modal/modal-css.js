7a14a746522388d4d9d04e3d778c1aa97e258c7e
Vaibhav <varora@catalystone.com>
2020-04-29 22:36:57 +0530

    #15 Add box-shadow of modal

--- react-exercises/modal-react/src/modal/modal-css.js
+++ react-exercises/modal-react/src/modal/modal-css.js
@@ -2,7 +2,8 @@ const modal = {
   overflowY: 'auto',
   border: '1px solid grey',
   borderRadius: '4px',
-  padding: '1rem'
+  padding: '1rem',
+  boxShadow: '0 11px 15px -7px rgba(0,0,0,.2),0 24px 38px 3px rgba(0,0,0,.14),0 9px 46px 8px rgba(0,0,0,.12)'
 }
 
 const floatingBox = {
