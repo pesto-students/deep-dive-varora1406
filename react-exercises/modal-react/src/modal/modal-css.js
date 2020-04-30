dc0c716681bb2d145daa3b7dfd834ec2b97eacf7
Vaibhav <varora@catalystone.com>
2020-04-30 20:19:50 +0530

    Add box-shadow in modal

--- react-exercises/modal-react/src/modal/modal-css.js
+++ react-exercises/modal-react/src/modal/modal-css.js
@@ -3,7 +3,8 @@ const modal = {
   border: '1px solid grey',
   borderRadius: '4px',
   padding: '1rem',
-  backgroundColor: 'white'
+  backgroundColor: 'white',
+  boxShadow: '0 11px 15px -7px rgba(0,0,0,.2),0 24px 38px 3px rgba(0,0,0,.14),0 9px 46px 8px rgba(0,0,0,.12)'
 }
 
 const modalCloseButton = {
