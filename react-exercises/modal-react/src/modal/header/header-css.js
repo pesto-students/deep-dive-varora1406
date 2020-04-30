58fcafc403f786450ab8fb6d1f4ca273ef7ab74a
Vaibhav <varora@catalystone.com>
2020-04-30 22:04:07 +0530

    #15 Add header component for modal

---
+++ react-exercises/modal-react/src/modal/header/header-css.js
@@ -0,0 +1,17 @@
+const modalHeader = {
+    display: 'flex',
+    justifyContent: 'flex-end',
+}
+
+const modalCloseButton = {
+    fontSize: '1.4rem',
+    fontWeight: 700,
+    lineHeight: 1,
+    color: '#000',
+    opacity: .3,
+    cursor: 'pointer',
+    border: 'none',
+    flex: 0
+}
+
+export { modalHeader, modalCloseButton }
---
+++ react-exercises/modal-react/src/modal/header/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { modalHeader, modalCloseButton } from './header-css';
+
+const Header = (props) => {
+    return (
+        <div style={modalHeader}>
+            <h3 style={{ flex: '1' }}>{props.children}</h3>
+            <button style={modalCloseButton} type="button" data-dismiss="modal" aria-label="Close" onClick={props.hideFunc}>
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    );
+}
+
+export { Header }
