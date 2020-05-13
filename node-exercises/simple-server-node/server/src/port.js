cc2be542e1cdd4309318bb7fee04c94ef2248e21
Vaibhav <varora@catalystone.com>
2020-05-13 22:28:45 +0530

    Add port utility
    Co-authored-by: Soumendu Prasad Sinha <soumsps@gmail.com>

---
+++ node-exercises/simple-server-node/server/src/port.js
@@ -0,0 +1,17 @@
+import is from 'is';
+
+const isValidPort = (port) => {
+  if (!is.number(port)) {
+    console.warn(`${port} is not a valid number`);
+    return false;
+  }
+
+  if (port < 1 || port > 65535) {
+    console.warn(`${port} has to be in range of 1 and 65535`);
+    return false;
+  }
+
+  return true;
+};
+
+export { isValidPort }
