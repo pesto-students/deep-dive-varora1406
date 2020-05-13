8d995b364acbf2588fe5580a54c2a1cd611e05a7
Vaibhav <varora@catalystone.com>
2020-05-14 00:09:54 +0530

    Add callback support in server

--- node-exercises/simple-server-node/server/src/server.js
+++ node-exercises/simple-server-node/server/src/server.js
@@ -3,23 +3,34 @@ const http = require('http');
 const { Route } = require('./route');
 
 class Server {
-  _pathList = [];
-
   constructor(port) {
+    this._routesList = [];
     if (!isValidPort(port)) {
       throw Error(`Can't start server without a valid port`);
     }
 
-    const httpServer = http.createServer();
+    const httpServer = http.createServer(this._serverCallback);
     httpServer.listen(port);
   }
 
-  get(path) {
-    this._pathList.push(new Route('get', path));
+  get(path, callback) {
+    this._routesList.push(new Route('get', path, callback));
   }
 
-  post(path) {
-    this._pathList.push(new Route('post', path));
+  post(path, callback) {
+    this._routesList.push(new Route('post', path, callback));
+  }
+
+  _serverCallback(request, response) {
+    //TODO: Fix issue of this object comig of HttpServer, and routesList is unable to access
+    console.log(this);
+    console.log(this._routesList);
+    const routesMatchingPath = this._routesList.filter(route => isPathMatch(request.url, route.path));
+    const routesMatchingPathAndMethod = routesMatchingPath.filter(route => route.method.toUpperCase() === request.method);
+
+    for (const route of routesMatchingPathAndMethod) {
+      route.callback.call(this, request, response);
+    }
   }
 }
 
