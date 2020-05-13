65078f78b755c672c800c31f6f938e94acb3b202
Vaibhav <varora@catalystone.com>
2020-05-13 22:32:11 +0530

    Add check on Server port
    Co-authored-by: Soumendu Prasad Sinha <soumsps@gmail.com>

--- node-exercises/simple-server-node/server/src/server.js
+++ node-exercises/simple-server-node/server/src/server.js
@@ -1,6 +1,10 @@
+import { isValidPort } from './port';
+
 class Server {
   constructor(port) {
-    
+    if (!isValidPort(port)) {
+      throw Error(`Can't start server without a valid port`);
+    }
   }
 
   get(path) {
