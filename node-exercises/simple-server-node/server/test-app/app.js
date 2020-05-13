914cbdc48797033c31fa3d2a09795c04353bfd0c
Vaibhav <varora@catalystone.com>
2020-05-14 00:07:23 +0530

    Add expectation of request, response in server callback

--- node-exercises/simple-server-node/server/test-app/app.js
+++ node-exercises/simple-server-node/server/test-app/app.js
@@ -2,10 +2,10 @@ const { Server } = require("../src/server");
 
 const server = new Server(8000);
 
-server.get('/path1', () => {
+server.get('/path1', (request, response) => {
   console.log('Request came');
 });
 
-server.post('/path1', () => {
+server.post('/path2', (request, response) => {
   console.log('Request came');
 });
