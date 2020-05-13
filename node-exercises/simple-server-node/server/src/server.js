378bae8ab9b3e880c902321d2928266fc5999563
Vaibhav <varora@catalystone.com>
2020-05-13 22:40:26 +0530

    Create server with http, using port available
    Co-authored-by: Soumendu Prasad Sinha <soumsps@gmail.com>

--- node-exercises/simple-server-node/server/src/server.js
+++ node-exercises/simple-server-node/server/src/server.js
@@ -1,10 +1,14 @@
 import { isValidPort } from './port';
+import http from 'http';
 
 class Server {
   constructor(port) {
     if (!isValidPort(port)) {
       throw Error(`Can't start server without a valid port`);
     }
+
+    const httpServer = http.createServer();
+    httpServer.listen(port);
   }
 
   get(path) {
