e053149ceaa74c593b65f2a1b09bd94b2aebdc52
Vaibhav <varora@catalystone.com>
2020-05-14 21:08:16 +0530

    Fix issue of pathIndex incase complete path is same

--- node-exercises/simple-server-node/server/src/path.js
+++ node-exercises/simple-server-node/server/src/path.js
@@ -1,6 +1,6 @@
 const isPathMatch = (url, path) => {
   // TODO: Do betterment in path matching
-  return url.indexOf(path) > 0;
+  return url.indexOf(path) >= 0;
 };
 
 module.exports = { isPathMatch };
