f922780730514f5c6f8854f2703bbaa78b331118
Vaibhav <varora@catalystone.com>
2020-05-14 21:09:41 +0530

    Enhance response callback with logs and end response

--- node-exercises/simple-server-node/server/test-app/app.js
+++ node-exercises/simple-server-node/server/test-app/app.js
@@ -3,9 +3,11 @@ const { Server } = require("../src/server");
 const server = new Server(8000);
 
 server.get('/path1', (request, response) => {
-  console.log('Request came');
+  console.log('Path1 Request came');
+  response.end();
 });
 
 server.post('/path2', (request, response) => {
-  console.log('Request came');
+  console.log('Path2 Request came');
+  response.end();
 });
