4e08403944d70e31a1e4df8ffd332e552a38c416
Vaibhav <varora@catalystone.com>
2020-05-16 13:32:35 +0530

    Add middleware support

--- node-exercises/simple-server-node/server/src/server.js
+++ node-exercises/simple-server-node/server/src/server.js
@@ -6,6 +6,7 @@ const { isPathMatch } = require('./path');
 class Server {
   constructor(port) {
     this.routesList = [];
+    this.middleWares = [];
 
     if (!isValidPort(port)) {
       this.port = 8000;
@@ -28,6 +29,10 @@ class Server {
     this.method('post', path, callback);
   }
 
+  use(callback) {
+    this.middleWares.push(callback);
+  }
+
   method(method, path, callback) {
     // TODO put a valid check to let know if methods are incorrect
     this.routesList.push(new Route(method, path, callback));
@@ -40,6 +45,10 @@ class Server {
     const routesMatchingPathAndMethod = routesMatchingPath.filter(
       (route) => route.method.toUpperCase() === request.method
     );
+
+    for (const middleWare of this.middleWares) {
+      middleWare.call(this, request, response);
+    }
 
     for (const route of routesMatchingPathAndMethod) {
       route.callback.call(this, request, response);
--- node-exercises/simple-server-node/server/tests/server.test.js
+++ node-exercises/simple-server-node/server/tests/server.test.js
@@ -3,7 +3,7 @@ const { Server } = require('../src/server');
 
 jest.setTimeout(30000);
 describe('Test server library', () => {
-  it(`should respond to requests`, async (done) => {
+  it(`should give response to requests`, async (done) => {
     const server = new Server(8001);
 
     const jestFn = jest.fn();
@@ -21,6 +21,30 @@ describe('Test server library', () => {
     const secondRequest = await request('http://localhost:8001').get('/yahoo');
     expect(secondRequest.status).toBe(200);
     expect(jestFn).toHaveBeenCalledTimes(3);
+
+    server.close();
+    done();
+  });
+
+  it(`should run middleWares`, async (done) => {
+    const server = new Server(8001);
+
+    const jestFn = jest.fn();
+    const callback = (_, res) => {
+      jestFn();
+      res.end();
+    };
+    server.get('/', callback);
+    server.method('get', '/yahoo', callback);
+    server.use(callback);
+    server.use(callback);
+    server.use(callback);
+
+    await request('http://localhost:8001').get('/');
+    expect(jestFn).toHaveBeenCalledTimes(5);
+
+    await request('http://localhost:8001').get('/yahoo');
+    expect(jestFn).toHaveBeenCalledTimes(9);
 
     server.close();
     done();
