a81e5937099ce69b18d38c76529cdb8bce0cd7d1
Vaibhav <varora@catalystone.com>
2020-05-16 17:50:44 +0530

    feat: add tests for server static middleware

--- node-exercises/simple-server-node/server/tests/server.test.js
+++ node-exercises/simple-server-node/server/tests/server.test.js
@@ -1,10 +1,13 @@
+import path from 'path';
+
 const request = require('supertest');
+const { ServerStatic } = require('../src/serve-static');
+
 const { Server } = require('../src/server');
 
-jest.setTimeout(30000);
 describe('Test server library', () => {
   it(`should give response to requests`, async (done) => {
-    const server = new Server(8001);
+    const server = new Server(8790);
 
     const jestFn = jest.fn();
     const callback = (_, res) => {
@@ -14,11 +17,11 @@ describe('Test server library', () => {
     server.get('/', callback);
     server.method('get', '/yahoo', callback);
 
-    const firstRequest = await request('http://localhost:8001').get('/');
+    const firstRequest = await request('http://localhost:8790').get('/');
     expect(firstRequest.status).toBe(200);
     expect(jestFn).toHaveBeenCalledTimes(2);
 
-    const secondRequest = await request('http://localhost:8001').get('/yahoo');
+    const secondRequest = await request('http://localhost:8790').get('/yahoo');
     expect(secondRequest.status).toBe(200);
     expect(jestFn).toHaveBeenCalledTimes(3);
 
@@ -27,7 +30,7 @@ describe('Test server library', () => {
   });
 
   it(`should run middleWares`, async (done) => {
-    const server = new Server(8001);
+    const server = new Server(8790);
 
     const jestFn = jest.fn();
     const callback = (_, res) => {
@@ -40,13 +43,33 @@ describe('Test server library', () => {
     server.use(callback);
     server.use(callback);
 
-    await request('http://localhost:8001').get('/');
+    await request('http://localhost:8790').get('/');
     expect(jestFn).toHaveBeenCalledTimes(5);
 
-    await request('http://localhost:8001').get('/yahoo');
+    await request('http://localhost:8790').get('/yahoo');
     expect(jestFn).toHaveBeenCalledTimes(9);
 
     server.close();
     done();
+  });
+
+  // TODO: Make more better tests for static files
+  it(`should serve static files`, (done) => {
+    const server = new Server(8790);
+
+    server.use(
+      new ServerStatic(path.join(__dirname, './example-test.txt')),
+      '/file'
+    );
+
+    request('http://localhost:8790')
+      .get('/file')
+      .then((response) => {
+        expect(response.text).toBe('example test');
+      })
+      .finally(() => {
+        server.close();
+        done();
+      });
   });
 });
