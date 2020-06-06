0ec3fe2f1d84347ad92981112e65b263d38443d8
Vaibhav <varora@catalystone.com>
2020-06-06 14:00:55 +0530

    docs: ✏️ update examples in comments - ideas in progress

--- react-exercises/simple-anime-react/flow/example/src/App.js
+++ react-exercises/simple-anime-react/flow/example/src/App.js
@@ -15,11 +15,36 @@ const App = () => {
   </>
 }
 
-// Slide up example
-{/* <Flow.Slide>
-  <Flow.Slide.Up time='0.5' auto-reverse={true}>
+// Slide examples
+
+// -- #Example 1
+{/*
+  <Flow.SlideUp (optional)time='0.5' >
     // content children
-  </Flow.Slide.Up>
-</Flow.Slide> */}
+  </Flow.SlideUp> 
+*/}
+
+// -- #Example 2
+{/*
+  <Flow.SlideDown (optional)time='0.5'>
+    // content children
+  </Flow.SlideDown>
+*/}
+
+// -- #Example 3
+{/*
+  <Flow.Slide direction='up | down'>
+    // content children
+  </Flow.Slide>
+*/}
+
+// -- (Discarded Idea for now) #Example 4
+{/*
+  <Flow.SlideUp flow=true>
+    <Flow.SlideDown flow=false>
+      // content children
+    </Flow.SlideDown>
+  </Flow.Slide>
+*/}
 
 export default App
